748ec5bd3fd2ea050d764af590888b1926cbb406
haerxiong <haerxiong@gmail.com>
2017-12-04 17:37:16 +0800

    Signed-off-by: haerxiong <haerxiong@gmail.com>

--- httpget/ali/tb_run.js
+++ httpget/ali/tb_run.js
@@ -4,7 +4,7 @@ var co = require('co');
 var fs = require('fs');
 var iconv = require('iconv-lite');
 
-var json = function(id){
+exports.json = function(id){
 	return new Promise(function(resove, reject){
 		var url = "https://detailskip.taobao.com/service/getData/1/p1/item/detail/sib.htm?itemId="+id+"&sellerId=202302469&modules=dynStock,qrcode,viewer,price,duty,xmpPromotion,delivery,upp,activity,fqg,zjys,couponActivity,soldQuantity,originalPrice,tradeContract&callback=onSibRequestSuccess";
 		request({
@@ -21,13 +21,12 @@ var json = function(id){
 			// 原始库存、价格信息
 			// fs.writeFile(id+'.json', res);
 			var info = JSON.parse(res);
-			console.log(info.data.dynStock.sellableQuantity);
-			resove();
+			resove(info);
 		});
 	});
 }
 
-var html = function(id){
+exports.html = function(id){
 	return new Promise(function(resove, reject){
 		var url = "https://item.taobao.com/item.htm?id="+id;
 		request({
@@ -38,12 +37,46 @@ var html = function(id){
 		}, function(err, response, body) {
 			var rs = iconv.decode(new Buffer(body), 'gbk');
 			// console.log(rs);
-			$ = cheerio.load(rs,{decodeEntities: false});
-			
 			// fs.writeFile(id+'.html', rs);
-			resove();
+			$ = cheerio.load(rs,{decodeEntities: false});
+
+			// 解析页面
+			var dtl = {
+				title : $(".tb-main-title").html().trim(),
+				rate : $("#J_RateCounter").html().trim(),
+				sale : $("#J_SellCounter").html().trim(),
+				imgs : new Array,
+				spec : new Array
+			};
+			// 解析缩略图
+			$("#J_UlThumb li").each(function(i, e){
+				dtl.imgs.push($(e).find("img").attr("data-src"));
+			});
+			// 解析规格
+			$("#J_isku dl.J_Prop").each(function(i, e){
+				var ary = {};
+				$(e).find("li").each(function(ii, ee){
+					ary[$(ee).attr('data-value')] = $(ee).find("a").text().trim();
+				});
+				dtl.spec.push({
+					"type" : $(e).find("dt").html(),
+					"ary" : ary
+				});
+			});
+			resove(dtl);
 		});
 	});
 }
 
-html("531414649886");
+var self = this;
+var gid = "531414649886";
+var test = function() {
+	self.json(gid).then(function(value){
+		var sku = value.data.dynStock.sku;
+		self.html(gid).then(function(dtl){
+			// console.log(dtl.spec[0].ary);
+			console.log(sku);
+		});
+	});
+}
+// test();
--- httpget/ali/tm_run.js
+++ httpget/ali/tm_run.js
@@ -4,7 +4,7 @@ var co = require('co');
 var fs = require('fs');
 var iconv = require('iconv-lite');
 
-var json = function(id){
+exports.json = function(id){
 	return new Promise(function(resove, reject){
 		var url = "https://mdskip.taobao.com/core/initItemDetail.htm?isPurchaseMallPage=false&isUseInventoryCenter=false&isAreaSell=false&tmallBuySupport=true&sellerPreview=false&queryMemberRight=false&tryBeforeBuy=false&cartEnable=true&isRegionLevel=false&cachedTimestamp=1511815191588&offlineShop=false&itemId="
 			+id+"&isForbidBuyItem=false&household=false&showShopProm=false&isApparel=true&addressLevel=3&service3C=false&isSecKill=false&callback=setMdskip&timestamp=1511850455693&isg=null&isg2=AsXFMKzMM1kyMReFNoIGMsIv1AE_Kni5IXFOl8cqzPwLXuXQj9KJ5FM2HJ1J&ref=https%3A%2F%2Fwww.tmall.com%2F%3Fspm%3Da220o.1000855.a2226mz.1.46a363e4SqNZOb";
@@ -14,22 +14,23 @@ var json = function(id){
 		    encoding: null,
 		    method: 'get',
 		    headers: {
-		    	"referer": "https://detail.tmall.com/item.htm?id=18644796859&skuId="+id
+		    	"referer": "https://detail.tmall.com/item.htm?id="+id
 		    }
 		}, function(err, response, body) {
-			var res = unescape(body.toString().replace(/\\u/g, '%u'));
+			var res = iconv.decode(new Buffer(body), 'gbk');
+			// var res = unescape(body.toString().replace(/\\u/g, '%u'));
 			res = res.substring(res.indexOf("{"), res.lastIndexOf("}")+1);
 			// 原始库存、价格信息
-			fs.writeFile(id+'.json', res);
+			// fs.writeFile(id+'.json', res);
 			var info = JSON.parse(res);
-			resove();
+			resove(info);
 		});
 	});
 }
 
-var html = function(id){
+exports.html = function(id){
 	return new Promise(function(resove, reject){
-		var url = "https://detail.tmall.com/item.htm?id=18644796859&skuId="+id;
+		var url = "https://detail.tmall.com/item.htm?id="+id;
 		request({
 		    url: url,
 		    // gzip: true,
@@ -46,5 +47,15 @@ var html = function(id){
 	});
 }
 
-// html("542192640711");
-json("542192640711");
+var self = this;
+var gid = "41320110185";
+var test = function() {
+	self.json(gid).then(function(value){
+		var q = value.defaultModel.inventoryDO;
+		console.log(q);
+		/*self.html(gid).then(function(dtl){
+			console.log(sku);
+		});*/
+	});
+}
+test();
